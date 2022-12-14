64ca25d23c22dd7650b3040aebc5a3dc2bc5544d
Banksy <445668+tcmsimoes@users.noreply.github.com>
2022-12-14 07:19:46 +0100

    updated tooltip handling

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BetterKeystones
-World of Warcraft plugin that provides several improvements to Mythic+ Keystones
+World of Warcraft addon that provides several improvements to Mythic+ Keystones:
+* Auto key slot
+* Replies with key when !key is mentioned in chat (party/guild)
+* Improves LFG to include leader RIO and applicants RIO (required RaiderIO to be present)
+* Overrides dungeon timer to include +3 and +2 limits
+* Add mob percentage to tooltip (requires MythicDungeonTools to be present)
--- TooltipPercentage.lua
+++ TooltipPercentage.lua
@@ -1,64 +1,55 @@
 local progressCache = {}
-local isTeemingWeek
 
-local function OnTooltipSetUnit(tooltip)
-    local scenarioType = select(10, C_Scenario.GetInfo())
-    if scenarioType == LE_SCENARIO_TYPE_CHALLENGE_MODE then
-        local name, unit = tooltip:GetUnit()
-        if unit and not UnitIsPlayer(unit) and UnitCanAttack("player", unit) then
-            local guid = unit and UnitGUID(unit)
-            if guid then
-                local value, total
-                local type, zero, serverId, instanceId, zoneUid, npcId, spawnUid = strsplit("-", guid)
-                local npcId = tonumber(npcId)
-                local npcProgressCache = progressCache[npcId]
-                if npcProgressCache then
-                    value = npcProgressCache[0]
-                    total = npcProgressCache[1]
-                elseif MDT then
-                    if isTeemingWeek == nill then
-                        isTeemingWeek = false
-                        local _, affixes = C_ChallengeMode.GetActiveKeystoneInfo()
-                        for _, affixId in ipairs(affixes) do
-                            if affixId == 5 then
-                                isTeemingWeek = true
-                            end
-                        end
-                    end
+local function OnTooltipSetUnit(tooltip, tooltipData)
+    if not MDT then
+        return
+    end
 
-                    local count, max, maxTeeming, teemingCount = MDT:GetEnemyForces(npcId)
+    local guid = tooltipData.guid
+    local unit = UnitTokenFromGUID(guid)
+    if unit and not UnitIsPlayer(unit) and UnitCanAttack("player", unit) then
+        local count, max
+        local type, zero, serverId, instanceId, zoneUid, npcId, spawnUid = strsplit("-", guid)
+        local npcProgressCache = progressCache[npcId]
+        if npcProgressCache then
+            count = npcProgressCache[0]
+            max = npcProgressCache[1]
+        else
+            local scenarioType = select(10, C_Scenario.GetInfo())
+            if scenarioType == LE_SCENARIO_TYPE_CHALLENGE_MODE then
+                count, max, _, _ = MDT:GetEnemyForces(npcId)
 
-                    if isTeemingWeek then
-                        value = count
-                        total = max
-                    else
-                        value = teemingCount
-                        total = maxTeeming
-                    end
-
+                if count and max then
                     npcProgressCache = {}
-                    npcProgressCache[0] = value
-                    npcProgressCache[1] = total
+                    npcProgressCache[0] = count
+                    npcProgressCache[1] = max
                     progressCache[npcId] = npcProgressCache
-                end
-
-                if value and total then
-                    local forcesMatcher = " - Enemy Forces: (%d+%%).*"
-
-                    for i=2, tooltip:NumLines() do
-                        local tiptext = _G["GameTooltipTextLeft"..i]
-                        local linetext = tiptext and tiptext:GetText()
-
-                        if linetext then
-                            for match in linetext:gmatch(forcesMatcher) do
-                                tiptext:SetText(format(" - Enemy Forces: %s | +%.2f%%", match, (value/total*100)))
-                                tooltip:Show()
-                            end
-                        end
-                    end
                 end
             end
         end
+
+        if count and max then
+            local newText = format(" - Enemy Forces: %d/%d|+%.2f%%", count, max, (count / max * 100))
+            local matched = false
+
+            for i = 2, tooltip:NumLines() do
+                local tooltipLine = _G["GameTooltooltipLineLeft"..i]
+                local lineText = tooltipLine and tooltipLine:GetText()
+
+                if lineText and lineText:match(" - Enemy Forces:.*") then
+                    tooltipLine:SetText(newText)
+                    matched = true
+                    break
+                end
+            end
+
+            if not matched then
+                tooltip:AddLine(newText)
+            end
+
+            tooltip:Show()
+        end
     end
 end
-GameTooltip:HookScript("OnTooltipSetUnit", OnTooltipSetUnit)
+
+TooltipDataProcessor.AddTooltipPostCall(Enum.TooltipDataType.Unit, OnTooltipSetUnit)
