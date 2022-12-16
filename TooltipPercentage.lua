80ac39fbdeba5a13a3002920716a5586ff8e9bcd
Banksy <445668+tcmsimoes@users.noreply.github.com>
2022-12-16 11:35:43 +0100

    reworked enemy percentage in tooltips

--- TooltipPercentage.lua
+++ TooltipPercentage.lua
@@ -1,53 +1,56 @@
 local progressCache = {}
 
 local function OnTooltipSetUnit(tooltip, tooltipData)
-    if not MDT then
-        return
-    end
-
     local guid = tooltipData.guid
     local unit = UnitTokenFromGUID(guid)
-    if unit and not UnitIsPlayer(unit) and UnitCanAttack("player", unit) then
-        local count, max
-        local type, zero, serverId, instanceId, zoneUid, npcId, spawnUid = strsplit("-", guid)
-        local npcProgressCache = progressCache[npcId]
-        if npcProgressCache then
-            count = npcProgressCache[0]
-            max = npcProgressCache[1]
-        else
-            local scenarioType = select(10, C_Scenario.GetInfo())
-            if scenarioType == LE_SCENARIO_TYPE_CHALLENGE_MODE then
-                count, max, _, _ = MDT:GetEnemyForces(npcId)
 
-                if count and max then
-                    npcProgressCache = {}
-                    npcProgressCache[0] = count
-                    npcProgressCache[1] = max
-                    progressCache[npcId] = npcProgressCache
+    if guid and unit then
+        local scenarioType = select(10, C_Scenario.GetInfo())
+        if scenarioType == LE_SCENARIO_TYPE_CHALLENGE_MODE then
+            local type, zero, server_id, instance_id, zone_uid, npc_id, spawn_uid = strsplit("-", guid)
+            npc_id = tonumber(npc_id)
+            local info = progressCache[npc_id]
+            if info then
+                local numCriteria = select(3, C_Scenario.GetStepInfo())
+                local total
+                local progressName
+                for criteriaIndex = 1, numCriteria do
+                    local criteriaString, _, _, quantity, totalQuantity, _, _, quantityString, _, _, _, _, isWeightedProgress = C_Scenario.GetCriteriaInfo(criteriaIndex)
+                    if isWeightedProgress then
+                        progressName = criteriaString
+                        total = totalQuantity
+                    end
+                end
+
+                local value, valueCount
+                if info then
+                    for amount, count in pairs(info) do
+                        if not valueCount or count > valueCount or (count == valueCount and amount < value) then
+                            value = amount
+                            valueCount = count
+                        end
+                    end
+                end
+
+                if value and total then
+                    local forcesFormat = format(" - %s: %%s", progressName)
+                    local text = format( format(forcesFormat, "+%.2f%% - +%d"), value/total*100, value)
+
+                    if text then
+                        local matcher = format(forcesFormat, "%d+%%")
+                        for i=2, tooltip:NumLines() do
+                            local tooltipText = _G["GameTooltipTextLeft"..i]
+                            local lineText = tooltipText and tooltipText:GetText()
+
+                            if lineText and lineText:match(matcher) then
+                                tooltipText:SetText(text)
+                                tooltip:Show()
+                                break
+                            end
+                        end
+                    end
                 end
             end
-        end
-
-        if count and max then
-            local newText = format(" - Enemy Forces: %d/%d|+%.2f%%", count, max, (count / max * 100))
-            local matched = false
-
-            for i = 2, tooltip:NumLines() do
-                local tooltipLine = _G["GameTooltooltipLineLeft"..i]
-                local lineText = tooltipLine and tooltipLine:GetText()
-
-                if lineText and lineText:match(" - Enemy Forces:.*") then
-                    tooltipLine:SetText(newText)
-                    matched = true
-                    break
-                end
-            end
-
-            if not matched then
-                tooltip:AddLine(newText)
-            end
-
-            tooltip:Show()
         end
     end
 end
